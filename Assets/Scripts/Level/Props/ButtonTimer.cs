12532ba5838a200ea50ad4cdfc86d47af75cc3aa
Captain Cobbs <egamezhq@gmail.com>
2025-02-28 02:55:02 -0700

    Added button timer class

--- Assets/Scripts/Level/Props/ButtonTimer.cs
+++ Assets/Scripts/Level/Props/ButtonTimer.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Level.Props
 {
-    internal class ButtonTimer
+    public class ButtonTimer
     {
+        [Header("Sprite")]
+        List<Sprite> Sprites;
+        [NonSerialized] public int TimeRemaining;
+
     }
 }
